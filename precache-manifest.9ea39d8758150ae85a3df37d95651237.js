3fc330722368fdfacc97b34e3e327bacf5538f33
Joehill27 <josephmhill96@gmail.com>
2019-11-23 10:13:16 -0500

    Updates

---
+++ precache-manifest.9ea39d8758150ae85a3df37d95651237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de91d6879292aeaf8bd63971b37ba97",
+    "url": "/Squires-Knights-In-the-Making/index.html"
+  },
+  {
+    "revision": "29d90ed53890bd7f488f",
+    "url": "/Squires-Knights-In-the-Making/static/css/main.e293d854.chunk.css"
+  },
+  {
+    "revision": "adf2ab7851f997318530",
+    "url": "/Squires-Knights-In-the-Making/static/js/2.5f95989b.chunk.js"
+  },
+  {
+    "revision": "29d90ed53890bd7f488f",
+    "url": "/Squires-Knights-In-the-Making/static/js/main.70a8d55d.chunk.js"
+  },
+  {
+    "revision": "fdc3b7f7f64e210c2da8",
+    "url": "/Squires-Knights-In-the-Making/static/js/runtime-main.6b443ffa.js"
+  },
+  {
+    "revision": "1cfc1bf1526bd08e807cc5e784137954",
+    "url": "/Squires-Knights-In-the-Making/static/media/Icon.1cfc1bf1.svg"
+  }
+]);
