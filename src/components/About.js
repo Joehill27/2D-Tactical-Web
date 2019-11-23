63e6fba45a868d42a12c7306ef4d5ac629fcc47b
Joehill27 <josephmhill96@gmail.com>
2019-11-23 11:40:06 -0500

    Updated about and assets.

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import '../App.css';
 import NavBar from './NavBar';
+import { Typography } from '@material-ui/core';
 
 
 const About = () => {
     return (
         <div>
             <NavBar/>
-            <div className="PageContent">
-                <h1>About</h1>
+            <div className="Home">
+                <div className="About">
+                    <Typography variant="h5">
+                        About
+                    </Typography>
+                    <div className="Paragraph">
+                        <Typography variant="subtitle1">
+                            What is Squires: Knights in the Making?
+                        </Typography>
+                        <Typography variant="body2">
+                            2D turn-based tactical shooter inspired by games like Worms, Shellshock Live, and Wild Ones. Built with Unity version 2019.3.0a2 in 2D. The three creators, Alex, Fernando, and Joseph, created this game as a semester long school project. To learn more, visit the home page for blog posts on progress updates, look at the source code on github, or play the game online! We tried to make the game as fun as possible, with references to pop culture. We do not own any of the assets on this website or used in the game, we just made the game for entertainment value. Have fun playing!
+                        </Typography>
+                    </div>
+                    <div className="Paragraph">
+                        <Typography variant="subtitle1">
+                            How does the game work?
+                        </Typography>
+                        <Typography variant="body2">
+                            2 or more teams comprising of 2 or more players are randomly spawned on a randomly generated map. Each team can be chosen to be entirely made of AI players or controlled by a human player. A player from each team will take a turn until all teams have gone once, then the next player on each team will take their turn. This procedure is followed until there is one team left, or the maximum time is reached. If a time limit is set, then the team with the highest total health is the winner.
+                        </Typography>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Assets.js
+++ src/components/Assets.js
@@ -28,38 +28,60 @@ const Assets = () => {
         <NavBar/>
             <div className="Home">
                 <div className={classes.root}>
-                    <div>
-                        <Typography variant="h3">Environment Hazards</Typography>
-                        <Grid container spacing={1}>
-                            <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs" src="https://i.imgur.com/Ak7RM9R.png" alt=""></img>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs"  src="https://i.imgur.com/bLqaWMA.png" alt=""></img>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs" src="https://i.imgur.com/eT9Tijr.png" alt=""></img>
-                                </Grid>
-                            </Grid>
-                            <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs" src="https://i.imgur.com/ooErwiH.png" alt=""></img>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs" src="https://i.imgur.com/EwXcC3M.png" alt=""></img>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <img className="Asset-Imgs" src="https://i.imgur.com/lVzRBui.png" alt=""></img>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </div>
-                    <div>
+                    <div className="Gray-Background">
                         <Typography variant="h3">Weapons</Typography>
                         <Grid container spacing={1}>
                             <Grid container item xs={12} spacing={3}>
                                 <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/0nCj7Ya.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs"  src="https://i.imgur.com/0fvbYsW.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/uvMHXkG.png" alt=""></img>
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={12} spacing={3}>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/cNbM6fu.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/zaEEMvl.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/kGmXxOI.png" alt=""></img>
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={12} spacing={3}>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/R13vVZ4.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/oAPghla.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/6fPsbrt.png" alt=""></img>
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={12} spacing={3}>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/aMv7cP2.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/3BMT5pY.png" alt=""></img>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <img className="Asset-Imgs" src="https://i.imgur.com/Kx1nqKR.png" alt=""></img>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </div>
+                    <div className="Gray-Background">
+                        <Typography variant="h3">Environment</Typography>
+                        <Grid container spacing={1}>
+                            <Grid container item xs={12} spacing={3}>
+                                <Grid item xs={4}>
                                     <img className="Asset-Imgs" src="https://i.imgur.com/Ak7RM9R.png" alt=""></img>
                                 </Grid>
                                 <Grid item xs={4}>
@@ -82,6 +104,7 @@ const Assets = () => {
                             </Grid>
                         </Grid>
                     </div>
+                    
                 </div>
             </div>
         </div>
